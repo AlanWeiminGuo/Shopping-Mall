fee530eddf326edce90bb978fe9672b1e6c45177
Weimin Guo <115796277+AlanWeiminGuo@users.noreply.github.com>
2022-10-15 11:47:58 +0100

    code

--- src/main.js
+++ src/main.js
@@ -2,5 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 1、引入normalize样式解决浏览器样式统一问题
+import 'normalize.css'
+// 2、引入初始化base.scss样式文件 1rem = 100px
+import './style/base.scss'
 
 createApp(App).use(store).use(router).mount('#app')
